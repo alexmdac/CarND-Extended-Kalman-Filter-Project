294565b283a45329476412408767f7a371b53199
Alex Mendes da Costa <alexmdac@gmail.com>
2017-09-19 01:14:58 -0700

    Fix indentiation

--- src/tools.cpp
+++ src/tools.cpp
@@ -31,27 +31,27 @@ VectorXd CalculateRMSE(
 }
 
 MatrixXd CalculateJacobian(const VectorXd& x_state) {
-	float px = x_state(0);
-	float py = x_state(1);
-	float vx = x_state(2);
-	float vy = x_state(3);
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
 
-	float c1 = px*px + py*py;
-	float c2 = sqrt(c1);
-	float c3 = c1 *c2;
- 
-	MatrixXd Hj(3, 4);
+  float c1 = px*px + py*py;
+  float c2 = sqrt(c1);
+  float c3 = c1 *c2;
 
-	if (fabs(c1) < 0.0001) {
-		cerr << "CalculateJacobian () - Error - Division by Zero" << endl;
-		return Hj;
-	}
+  MatrixXd Hj(3, 4);
 
-	Hj << (px/c2),               (py/c2),               0,     0,
+  if (fabs(c1) < 0.0001) {
+    cerr << "CalculateJacobian () - Error - Division by Zero" << endl;
+    return Hj;
+  }
+
+  Hj << (px/c2),               (py/c2),               0,     0,
        -(py/c1),               (px/c1),               0,     0,
-		    py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+        py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
 
-	return Hj;
+  return Hj;
 }
 
 MatrixXd CalculateProcessCovarianceMatrix(
